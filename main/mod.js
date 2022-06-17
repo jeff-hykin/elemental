55131fe64dad4e0ba8c0ef582a8540679d6d373c
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-17 10:50:42 -0500

    -

--- main/mod.js
+++
@@ -1,5 +0,0 @@
-export const h = (...args)=>console.log("h",args)
-export const render = (...args)=>console.log("render",args)
-export const hydrate = (...args)=>console.log("hydrate",args)
-export const renderSSR = (...args)=>console.log("renderSSR",args)
-export default { h, render, hydrate, renderSSR }
